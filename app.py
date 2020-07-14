ae64bc02ae9cde71b3b268cfb3cbcd04d3a3b4d9
Sahil Fruitwala <sahilfruitwala007@gmail.com>
2020-07-14 20:08:58 -0300

    back to basic

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from os import environ
+import os
 from flask import Flask, jsonify, request, flash
 from flask_mail import Mail
 from flask_cors import CORS
@@ -9,22 +9,22 @@ import json
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'apikey'
-app.config['MAIL_PASSWORD'] = environ.get('SENDGRID_API_KEY')
-app.config['MAIL_DEFAULT_SENDER'] = environ.get('MAIL_DEFAULT_SENDER')
+app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 
 mail = Mail(app)
 CORS(app)
 bcrypt = Bcrypt(app)
 
-URI = URI = environ.get('MONGODB_URI_PART1') + '&' + environ.get('MONGODB_URI_PART1') # add db url
-client = MongoClient(URI)
-database = client.rentalvista
+MONGODB_URI = os.environ.get('MONGODB_URI_PART1') + '&' + os.environ.get('MONGODB_URI_PART2') # add db url
 
+client = MongoClient(MONGODB_URI)
+database = client.rentalvista
 
 @app.route("/signup", methods=["POST"])
 def signup():
